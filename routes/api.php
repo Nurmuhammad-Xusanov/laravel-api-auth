506c711f5f9e7bf49a343e7e56fc48887d423e67
Nurmuhammad <nursherzil@gmail.com>
2025-12-27 11:29:33 +0500

    Add API rate limiting

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', [App\Http\Controllers\AuthController::class, 'register']);
-Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
+Route::middleware('throttle:api')->group(function () {
+    Route::post('register', [App\Http\Controllers\AuthController::class, 'register']);
+    Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('logout', [App\Http\Controllers\AuthController::class, 'logout']);
-    Route::apiResource('laptops', App\Http\Controllers\LaptopController::class);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [App\Http\Controllers\AuthController::class, 'logout']);
+        Route::apiResource('laptops', App\Http\Controllers\LaptopController::class);
+    });
 });
-
-?>
